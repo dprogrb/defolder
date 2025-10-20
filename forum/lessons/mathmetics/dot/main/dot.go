3ee9df6d505c90cf11a22a7d3d7ae121ce8bc94b
d_bl1n <programmer@defolder.com>
2025-10-20 23:44:30 +0700

    created lesson matmentics/dot

---
+++ forum/lessons/mathmetics/dot/main/dot.go
@@ -0,0 +1,15 @@
+components {
+  id: "point"
+  component: "/main/dot.script"
+}
+embedded_components {
+  id: "sprite"
+  type: "sprite"
+  data: "default_animation: \"anim\"\n"
+  "material: \"/builtins/materials/sprite.material\"\n"
+  "textures {\n"
+  "  sampler: \"texture_sampler\"\n"
+  "  texture: \"/builtins/graphics/particle_blob.tilesource\"\n"
+  "}\n"
+  ""
+}
