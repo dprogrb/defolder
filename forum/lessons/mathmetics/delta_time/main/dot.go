89199b0ab100451e69db20c37233acf5f9288a3e
d_bl1n <programmer@defolder.com>
2025-11-06 14:01:38 +0700

    created lesson the delta-time

---
+++ forum/lessons/mathmetics/delta_time/main/dot.go
@@ -0,0 +1,15 @@
+components {
+  id: "comparison"
+  component: "/scripts/comparison.script"
+}
+embedded_components {
+  id: "sprite"
+  type: "sprite"
+  data: "default_animation: \"anim\"\n"
+  "material: \"/builtins/materials/sprite.material\"\n"
+  "textures {\n"
+  "  sampler: \"texture_sampler\"\n"
+  "  texture: \"/builtins/graphics/particle_blob.tilesource\"\n"
+  "}\n"
+  ""
+}
